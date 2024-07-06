e65f8608619cc147a03c82f0c5d561c6487b3631
Popoola Temilorun <horluwatemilorunolamilekan@gmail.com>
2024-07-06 04:08:40 +0100

    login details

--- AuthOrg/settings.py
+++ AuthOrg/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -95,7 +95,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'Authorg',
         'USER': 'postgres',
-        'PASSWORD': 'leebackendengine',
+        'PASSWORD': os.environ.get('book hall postgres PASSWORD'),
         'HOST': 'localhost',
         'PORT': '5432',
     }
