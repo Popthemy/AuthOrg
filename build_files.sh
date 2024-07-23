c5d825e42cadf5effb5ca464ee344f3712192b04
Popoola Temilorun <horluwatemilorunolamilekan@gmail.com>
2024-07-23 14:37:51 +0100

    edited build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -6,8 +6,7 @@ echo "Starting build process..."
 # Install only the required dependencies
 python3.9 -m pip install -r requirements.txt || { echo 'Failed to install dependencies'; exit 1; }
 
-# Make migrations (if using Django)
-python3.9 manage.py makemigrations || { echo 'Failed to make migrations'; exit 1; }
+
 
 # Apply migrations (if using Django)
 python3.9 manage.py migrate || { echo 'Failed to apply migrations'; exit 1; }
