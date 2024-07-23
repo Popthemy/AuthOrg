754aff033d6735bd803ae66b2104a5aabae028ee
Popoola Temilorun <horluwatemilorunolamilekan@gmail.com>
2024-07-23 14:26:11 +0100

    changed appliation to app

--- AuthOrg/wsgi.py
+++ AuthOrg/wsgi.py
@@ -13,5 +13,5 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AuthOrg.settings.dev')
 
-application = get_wsgi_application()
+app = get_wsgi_application()
 
